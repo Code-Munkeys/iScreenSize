a257eab8ffa0472f31f9f6752ba8cf5e44684d9e
ayestaran <franz@ayestaran.co.uk>
2014-09-11 12:45:36 +0100

    Autolayout
    11/09/14

--- iScreenSize/ViewController.m
+++ iScreenSize/ViewController.m
@@ -37,31 +37,51 @@
     
     if ((height==736) && (width==414))
     {
-        lblScreenSize.text = @ "iPhone 6 Plus (5.5-inch 1920 x 1080)";
+        lblScreenSize.text = @ "iPhone 6 Plus [Potrait] (5.5-inch 1920 x 1080)";
+    }
+    
+    if ((height==414) && (width==736))
+    {
+        lblScreenSize.text = @ "iPhone 6 Plus [Landscape] (5.5-inch 1080 x 1920)";
     }
     
     if ((height==667) && (width==375))
     {
-        lblScreenSize.text = @ "iPhone 6 (4.7-inch 1334 x 750)";
+        lblScreenSize.text = @ "iPhone 6 [Potrait] (4.7-inch 1334 x 750)";
+    }
+
+    if ((height==375) && (width==667))
+    {
+        lblScreenSize.text = @ "iPhone 6 [Landscape] (4.7-inch 750 x 1334)";
     }
     
     if ((height==568) && (width==320))
     {
-        lblScreenSize.text = @ "iPhone 5/5S/5C (4-inch 1136 x 640)";
+        lblScreenSize.text = @ "iPhone 5/5S/5C [Potrait] (4-inch 1136 x 640)";
     }
-    
+
+    if ((height==320) && (width==568))
+    {
+        lblScreenSize.text = @ "iPhone 5/5S/5C [Landscape] (4-inch 640 x 1136)";
+    }
+
     if ((height==480) && (width==320))
     {
-        lblScreenSize.text = @ "iPhone 4/4S (3.5-inch 960 x 640)";
+        lblScreenSize.text = @ "iPhone 4/4S [Potrait] (3.5-inch 960 x 640)";
     }
-    
+
+    if ((height==320) && (width==480))
+    {
+        lblScreenSize.text = @ "iPhone 4/4S [Landscape] (3.5-inch 640 x 960)";
+    }
+
     if ((height==1024) && (width==768))
     {
-        lblScreenSize.text = @ "iPad 2/Air/Retina Portrait (9.7-inch 1024 x 768)";
+        lblScreenSize.text = @ "iPad 2/Air/Retina [Portrait] (9.7-inch 1024 x 768)";
     }
     if ((height==768) && (width==1024))
     {
-        lblScreenSize.text = @ "iPad 2/Air/Retina Landscape (9.7-inch 1024 x 768)";
+        lblScreenSize.text = @ "iPad 2/Air/Retina [Landscape] (9.7-inch 1024 x 768)";
     }
 }
 
